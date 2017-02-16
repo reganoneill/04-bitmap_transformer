b9bb4e884d19446cc5bb9e0696100dd635ad0e06
Dana Kulp <dkulp23@gmail.com>
2017-02-16 15:51:25 -0800

    Test to isolate pixel array, change it and rewrite it.

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 const fs = require('fs');
 const bitmap = fs.readFileSync(`${__dirname}/assets/palette-bitmap.bmp`);
 
+const bitmapUpdate = function(data) {
+  fs.writeFileSync(`${__dirname}/assets/dana-bitmap.bmp`, data);
+};
+
 const bmp = {};
 
 bmp.type = bitmap.toString('utf-8', 0, 2);
 bmp.size = bitmap.readInt32LE(2);
 bmp.width = bitmap.readInt32LE(18);
 bmp.height = bitmap.readInt32LE(22);
-bmp.pixelArray = bitmap.readInt32LE('hex', 10);
+bmp.pixelArray = bitmap.readInt32LE(10);
+bmp.imgHeader = bitmap.slice(0, 1078);
+bmp.imgBuffer = bitmap.slice(1078).reverse();
+bmp.imgConcat = bmp.imgHeader + bmp.imgBuffer;
 
-console.dir(bmp);
+bitmapUpdate(bmp.imgConcat);
+
+// console.dir(bmp);
