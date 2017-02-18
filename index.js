f26996c97262b3c0444747f0357d4216230b86d6
reganoneill <rkrotho@gmail.com>
2017-02-17 17:09:47 -0800

    combined group work

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
 'use strict';
 
+const EE = require('events');
+const ee = new EE();
 const fs = require('fs');
+const objConstructor = require(`${__dirname}/models/bitmap-object.js`).ImageObj;
 const bitmap = fs.readFileSync(`${__dirname}/assets/palette-bitmap.bmp`);
 
 const bitmapUpdate = function(data) {
@@ -9,10 +13,7 @@ const bitmapUpdate = function(data) {
 
 var colorTableArray = [];
 
-
-
 const bmp = {};
-
 bmp.type = bitmap.toString('utf-8', 0, 2);
 bmp.size = bitmap.readInt32LE(2);
 bmp.startOfPixelArray = bitmap.readInt32LE(10);
@@ -23,76 +24,22 @@ bmp.area = bitmap.readInt32LE(34);
 bmp.colorsInPalette = bitmap.readInt32LE(46);
 bmp.importantColors = bitmap.readInt32LE(50);
 bmp.colorTable = bitmap.toString('hex', 54, 1078);
-// var bigOlTableArraySTring = bmp.colorTable.match(/.{1,8}/g);
 var arr = [];
 var newColorzzz = bmp.colorTable.match(/\w{8}/g).forEach(grp => (arr).push(grp.replace(/^\w{2}/g, '00')));
 
 
 console.log('result', arr.join(''));
 
-//new functionality 4:29 - feb 17
-// var newStringFromColorTable = bmp.colorTable;
-// // console.log(bmp.colorTable);
-// var arr = []
-//
-// var x = newStringFromColorTable.match(/\w{8}/g).forEach(grp => (arr).push(grp.replace(/^\w{2}/g, '00')));
-// console.log(arr);
+ee.once('getFile', function() {
+  fs.readFile(`${__dirname}/assets/palette-bitmap.bmp`, function(err, data) {
+    if (err) throw err;
+    ee.emit('objCreate', data);
+  });
+});
 
-// console.dir(bmp);
-// console.log('color table array:', colorTableArray);
+ee.on('objCreate', function(data) {
+  let testObj = new objConstructor(data);
+  console.log('testObj:', testObj.colorsArray);
+});
 
-
-
-
-//this works but ditching for above
-// var bigOlTableArraySTring = bmp.colorTable.match(/.{1,8}/g);
-// console.log('chchchyea:', bigOlTableArraySTring);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// bmp.fileOffset2PixelArray = bitmap.readInt32LE(10);
-// // bmp.colorsUsed = bitmap.readInt32LE(42);
-// // bmp.colorsUsedHEX = bitmap.toString('hex',42);
-// bmp.colorTable = bitmap.readInt32LE(54);
-
-// bmp.colorTableHexxx = bitmap.toString('hex', 52, 58);
-//logging the offset i.e. starting address of the byte where the bitmap image data(pixel array) can be found
-// bmp.pixelArray = bitmap.readInt32LE(10);
-// bmp.colorArray = bitmap.slice(54, 1078);
-
-// bmp.getRBG = function(){};
-
-// bmp.imgHeader = bitmap.slice(0, 1078);
-// bmp.imgBuffer = bitmap.slice(1078).reverse();
-// bmp.imgConcat = bmp.imgHeader + bmp.imgBuffer;
-
-
-// console.log(bmp.pixelArray);
-// bitmapUpdate(bmp.imgConcat);
-
-
-// console.log(bitmap);
-// console.log('color array - from 54 to 1078', bmp.colorArray);
-
-// what is a 32 bit integer in relation to a rgb value?
-//color array = slice 54-1078
-//separate into parts of 4 bits then go through color array jumping 4's
+ee.emit('getFile');
---
+++ models/bitmap-object.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.ImageObj = function(buffer) {
+  this.type = buffer.toString('utf-8', 0, 2);
+  this.headerSize = buffer.readInt32LE(14);
+  this.imgWidth = buffer.readInt32LE(18);
+  this.imgHeight = buffer.readInt32LE(22);
+  this.colorPlanes = buffer.readInt16LE(26);
+  this.bitsPerPixel = buffer.readInt16LE(28);
+  this.imgSize = buffer.readInt32LE(34);
+  this.heightRez = buffer.readInt32LE(38);
+  this.vertRez = buffer.readInt32LE(42);
+  this.numColors = buffer.readInt32LE(46);
+  this.imptColors = buffer.readInt32LE(50);
+  // this.firstColor = buffer.toSting('hex', 54, 58);
+  this.secondColor = buffer.readInt32LE(58);
+  this.thirdColor = buffer.readInt32LE(62);
+  // this.colorsArray = buffer.slice('54, 1079').reduce(function(acc, ele) {
+  //   for (let i = 54; i < 257; i += 4) {
+  //     acc.push(ele.readInt32LE(i));
+  //   }
+  //   return acc;
+  // }, []);
+};
