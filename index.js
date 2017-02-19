06475522bd5310648e2130a03461dd7d603aa7c3
reganoneill <rkrotho@gmail.com>
2017-02-19 13:38:58 -0800

    first passing test

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const fs = require('fs');
 const objConstructor = require(`${__dirname}/models/bitmap-object.js`).ImageObj;
 const bmpTransformer = require(`${__dirname}/lib/bitmap-transform.js`);
 
-// var arr = [];
-
-// var newColorzzz = bmp.colorTable.match(/\w{8}/g).forEach(grp => (arr).push(grp.replace(/^\w{2}/g, '00')));
-//
-// console.log('result', arr.join(''));
-
 ee.on('getFile', function() {
   fs.readFile(`${__dirname}/assets/palette-bitmap.bmp`, function(err, data) {
     if (err) throw err;
--- test/buffer-test.js
+++ test/buffer-test.js
@@ -1,4 +1,39 @@
 'use strict';
+const chai = require('chai');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+const EventEmitter = require('events').EventEmitter;
+const bmpReader = require('../index.js');
 
-//Buffer.isBuffer(obj) test
-//Buffer.isEncoding(binary) testObj
+
+describe('EventEmitter', function(){
+  describe('#emit-event', function(){
+    it('should invoke callback chain', function(done){
+      var spy = sinon.spy();
+      var emitter = new EventEmitter;
+
+      emitter.on('getFile', spy);
+      emitter.emit('getFile');
+      // expect(spy).to.equal.true;
+      expect(spy).to.not.throw(Error);
+      done();
+    });
+  });
+});
+
+
+//this is hell
+// describe('Event Emitter', function(){
+//   describe('event is being emitted', function(){
+//       it('should return error for no callback', function(done){
+//         var spy = sinon.spy();
+//         var emitter = new EventEmitter;
+//         emitter.on('getFile', spy);
+//         emitter.emit('getFile');
+//         expect(spy).to.contain('bird')
+//         done();
+//         });
+//       });
+//   });
